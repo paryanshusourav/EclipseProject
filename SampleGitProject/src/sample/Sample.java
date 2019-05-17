20007752f36698adf924881eb74dde472a6b2e1d
paryanshu sourav <paryanshusourav@gmail.com>
2019-05-17 17:13:47 +0530

    calling mehid in main

--- SampleGitProject/src/sample/Sample.java
+++ SampleGitProject/src/sample/Sample.java
@@ -2,13 +2,14 @@ package sample;
 
 public class Sample {
 
-	public void m1()
+	public static void m1()
 	{
 		System.out.println("in method");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Sample.m1();
 	}
 
 }
