27e5718f9c7b5da4e20c5ee98fb68b171cce85fe
paryanshu sourav <paryanshusourav@gmail.com>
2019-05-17 16:13:23 +0530

    Adding a new project

---
+++ SampleGitProject/src/sample/Sampe.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Sampe {
+
+	public void m1()
+	{
+		System.out.println("in method");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
