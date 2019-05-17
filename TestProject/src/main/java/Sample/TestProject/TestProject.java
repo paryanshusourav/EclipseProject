79689ac3eb637fe31f215f5e14359c63bb1be8a4
paryanshu sourav <paryanshusourav@gmail.com>
2019-05-17 17:28:37 +0530

    Adding class file

---
+++ TestProject/src/main/java/Sample/TestProject/TestProject.java
@@ -0,0 +1,10 @@
+package Sample.TestProject;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In Main method");
+	}
+
+}
